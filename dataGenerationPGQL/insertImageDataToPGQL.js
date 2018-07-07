d85e8b3bd8c8b563b6c053c89697cdd20de38ba4
John M. Long <johnm.long@Johns-MacBook-Pro.local>
2018-07-06 21:18:46 -0700

    misc changes

--- dataGenerationPGQL/insertImageDataToPGQL.js
+++ dataGenerationPGQL/insertImageDataToPGQL.js
@@ -1,6 +1,29 @@
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/listings_db';
-const generateHost = './dataGenerator.js'
+//const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://18.220.20.69:5432/listings_db';
+//'postgres://localhost:5432/listings_db';
+//18.220.20.69
+//const generateHost = './dataGenerator.js'
+
+//const util = require('util');
+const { Client } = require('pg');
+//const {promisify} = require('util');
+
+const dbConfig = {
+  "host" : "18.217.133.223",
+  "database" : "listings_db",
+  "user" : "postgres",
+ // "password" : "",
+  "port" : 5432,
+}
+
+// const pool = new Pool(dbConfig);
+const client = new Client(dbConfig);
+
+// pool.on('error', (err, client) => {
+//   console.error('Unexpected error on idle client', err)
+//   process.exit(-1)
+// })
+
 
 const insertImageData = function() {
   let fileName = 'imageData.csv';
@@ -9,22 +32,16 @@ const insertImageData = function() {
 
 //~"+ __dirname + "
   console.log('query:', queryStr)
-  pg.connect(connectionString, (err, client, done) => {
-  // Handle connection errors
-    if(err) {
-      done();
-      console.log('error:', err);
-      return;
-    }
+  client.connect()
 
-    const query = client.query(queryStr, (err, result) => {
-        if(err){
-          console.log('insert err:', err)
-        } else {
-          console.log('saved to DB')
-        }
-      })
+  client.query(queryStr, (err, result) => {
+    if(err){
+      console.log('insert err:', err)
+    } else {
+      console.log('saved to DB')
+    }
   });
+
 }
 
 insertImageData()
